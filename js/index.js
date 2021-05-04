d974bd90ef7279bb8a169fc0c601a3fcd43d04b3
Maxime Layet <layetmaxime@gmail.com>
2021-05-04 13:39:17 +0200

    projet brito

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const text = document.querySelector("h2");
+
+function getChrono() {
+	const now = new Date().getTime();
+	const countDownDate = new Date("July 23, 2021").getTime();
+
+	const distanceBase = countDownDate - now;
+
+	const days = Math.floor(distanceBase / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((distanceBase % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distanceBase % (1000 * 60)) / 1000);
+
+	text.innerText = days + "j " + hours + "h " + minutes + "m " + seconds + "s ";
+}
+
+const countDownInterval = setInterval(() => {
+	getChrono();
+}, 1000);
